ba9d0c15033599a6164664ddfb79c792a75ae796
Кудрявцев Дмитрий Вальтерович <DVKudryavtsev@alfabank.ru>
2020-01-28 18:11:19 +0300

    add button actions + add error page

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import Button from 'arui-feather/button';
 import OkIcon from 'arui-feather/icon/ui/ok';
 import FailIcon from 'arui-feather/icon/ui/fail';
 import IconButton from 'arui-feather/icon-button';
+import Input from 'arui-feather/input';
+import cn from 'classname';
 
 import { CONTRACT_ADDRESS, CONTRACT_ABI, BALANCE_MINORITY } from '../constants/app';
 import './App.css';
@@ -19,18 +21,23 @@ const STATUSES = {
 
 const customStyles = {
     content : {
-        top                   : '50%',
-        left                  : '50%',
-        right                 : 'auto',
-        bottom                : 'auto',
-        marginRight           : '-50%',
-        transform             : 'translate(-50%, -50%)'
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        padding: '40px'
+    },
+    overlay: {
+        background: 'rgba(0,0,0,0.85)'
     }
 };
 
 class App extends Component {
     state = {
         contract: new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS),
+        currentAccount: undefined,
         owner: undefined,
         seller: undefined,
         sellerStatus: undefined,
@@ -38,7 +45,9 @@ class App extends Component {
         buyerStatus: undefined,
         balance: 0,
         isBalanceLoading: false,
-        isBalanceModalShow: false
+        isBalanceModalShow: false,
+        inputMoney: 0,
+        error: undefined
     };
 
     async componentDidMount() {
@@ -46,47 +55,93 @@ class App extends Component {
 
         window.ethereum.enable();
 
-        const owner = await contract.methods.owner().call();
-        const seller = await contract.methods.seller().call();
-        const buyer = await contract.methods.buyer().call();
-        const balance = await contract.methods.balance().call();
+        try {
+            const owner = (await contract.methods.owner().call()).toLowerCase();
+            const seller = (await contract.methods.seller().call()).toLowerCase();
+            const buyer = (await contract.methods.buyer().call()).toLowerCase();
+            const buyerStatus = (await contract.methods.buyerOk().call()) ? STATUSES.ACCEPT : undefined;
+            const sellerStatus = (await contract.methods.sellerOk().call()) ? STATUSES.ACCEPT : undefined;
+            const balance = await contract.methods.balance().call();
+            const currentAccount = web3.eth.accounts.currentProvider.selectedAddress.toLowerCase();
 
-        this.setState({ owner, seller, buyer, balance });
+            setInterval(() => {
+                const { currentAccount } = this.state;
+                const newAccount = web3.eth.accounts.currentProvider.selectedAddress.toLowerCase();
+                if (newAccount !== currentAccount) {
+                    this.setState({ currentAccount: newAccount });
+                }
+            }, 500);
+
+            this.setState({ owner, seller, sellerStatus, buyer, buyerStatus, balance, currentAccount }, () => console.log(this.state));
+        } catch (error) {
+            this.setState({ error })
+        }
+
     }
 
     handleDeposit = async () => {
-        const { contract, buyer } = this.state;
+        const { contract, currentAccount, inputMoney } = this.state;
 
         this.setState({ isBalanceLoading: true });
 
         await contract.methods.deposit().send({
-            from: buyer,
-            value: web3.utils.toWei('0.1', 'ether')
+            from: currentAccount,
+            value: web3.utils.toWei(inputMoney, 'ether')
         });
         const balance = await contract.methods.balance().call();
 
-        this.setState({ isBalanceLoading: false, balance });
+        this.setState({ isBalanceLoading: false, isBalanceModalShow: false, balance });
     };
 
-    handleAccept = (address) => {
-        const { contract } = this.state;
+    handleChangeStatus = async (address, isAccepted) => {
+        const { contract, seller, sellerStatus, buyer, buyerStatus, currentAccount } = this.state;
 
+        if (currentAccount !== address) {
+            return;
+        }
+
+        if (isAccepted) {
+            await contract.methods.accept().send({ from: currentAccount });
+            if (buyerStatus === STATUSES.ACCEPT && sellerStatus === STATUSES.ACCEPT){
+                alert(`Все средства были переведены продавцу ${seller}`);
+            }
+        } else {
+            await contract.methods.reject().call();
+            alert(`Все средства были возращены покупателю ${buyer}`);
+        }
+
+        if (address === buyer) {
+            this.setState({ buyerStatus: isAccepted ? STATUSES.ACCEPT : STATUSES.REJECT });
+        } else if (address === seller) {
+            this.setState({ sellerStatus: isAccepted ? STATUSES.ACCEPT : STATUSES.REJECT });
+        }
     };
 
-    handleReject = (address) => {
+    handleAccept = (address) => () => this.handleChangeStatus(address, true);
+    handleReject = (address) => () => this.handleChangeStatus(address, false);
 
-    };
+    openBalanceModal = () => this.setState({ isBalanceModalShow: true });
+    closeBalanceModal = () => this.setState({ isBalanceModalShow: false });
 
-    openBalanceModal = () => {
-        this.setState({ isBalanceModalShow: true });
-    };
-
-    closeBalanceModal = () => {
-        this.setState({ isBalanceModalShow: false });
-    };
+    handleChangeMoneyInput = (inputMoney) => this.setState({ inputMoney });
 
     render() {
-        const { owner, seller, buyer, balance, isBalanceLoading, isBalanceModalShow } = this.state;
+        const {
+            error, owner, seller, buyer, balance, isBalanceLoading, isBalanceModalShow,
+            buyerStatus, sellerStatus, currentAccount
+        } = this.state;
+
+        if (error) {
+            return (
+                <div className='app app__error'>
+                    <h1>
+                        Ошибка. Контракт не действителен.<br/>
+                        Создайте новый и обновите приложение.
+                    </h1>
+                    <img src='https://cdn.oubly.net/img/the_dev_dog.gif' />
+                </div>
+            )
+        }
 
         const ethBalance = balance && window.web3.fromWei(balance, 'ether');
 
@@ -103,33 +158,61 @@ class App extends Component {
                                         <div />
                                     </td>
                                 </tr>
-                                <tr>
+                                <tr className={ cn({'app__info__table__line--disabled': currentAccount !== buyer}) } >
                                     <td className='app__info__table__key'>Покупатель:</td>
                                     <td className='app__info__table__value'>{ buyer }</td>
                                     <td className='app__info__table__actions'>
                                         {
                                             buyer && (
                                                 <div>
-                                                    <IconButton onClick={ this.handleAccept(buyer)}>
-                                                        <OkIcon colored={ true } size='l'/>
+                                                    <IconButton
+                                                        disabled={ currentAccount !== buyer }
+                                                        onClick={ this.handleAccept(buyer)}
+                                                    >
+                                                        <OkIcon
+                                                            className={ buyerStatus === STATUSES.ACCEPT ? 'icon__approve--pressed' : '' }
+                                                            size='l'
+                                                        />
                                                     </IconButton>
-                                                    <IconButton onClick={ this.handleReject(buyer)}>
-                                                        <FailIcon size='l'/>
+                                                    <IconButton
+                                                        disabled={ currentAccount !== buyer }
+                                                        onClick={ this.handleReject(buyer)}
+                                                    >
+                                                        <FailIcon
+                                                            className={ buyerStatus === STATUSES.REJECT ? 'icon__reject--pressed' : '' }
+                                                            size='l'
+                                                        />
                                                     </IconButton>
                                                 </div>
                                             )
                                         }
                                     </td>
                                 </tr>
-                                <tr>
+                                <tr className={ cn({'app__info__table__line--disabled': currentAccount !== seller}) }>
                                     <td className='app__info__table__key'>Продавец:</td>
                                     <td className='app__info__table__value'>{ seller }</td>
                                     <td className='app__info__table__actions'>
                                         {
                                             seller && (
                                                 <div>
-                                                    <IconButton><OkIcon colored={ true } size='l'/></IconButton>
-                                                    <IconButton><FailIcon size='l'/></IconButton>
+                                                    <IconButton
+                                                        disabled={ currentAccount !== seller }
+                                                        onClick={ this.handleAccept(seller)}
+                                                    >
+                                                        <OkIcon
+                                                            className={ sellerStatus === STATUSES.ACCEPT ? 'icon__approve--pressed' : '' }
+                                                            size='l'
+                                                        />
+                                                    </IconButton>
+                                                    <IconButton
+                                                        disabled={ currentAccount !== seller }
+                                                        onClick={ this.handleReject(seller)}
+                                                    >
+                                                        <FailIcon
+                                                            className={ sellerStatus === STATUSES.REJECT ? 'icon__reject--pressed' : '' }
+                                                            size='l'
+                                                        />
+                                                    </IconButton>
                                                 </div>
                                             )
                                         }
@@ -161,25 +244,35 @@ class App extends Component {
                                 disabled={ isBalanceLoading }
                                 onClick={ this.openBalanceModal }
                             >
-                                { isBalanceLoading ? 'Идет оплата...' : 'Внести деньги' }
+                                { isBalanceLoading ? 'Идет оплата...' : 'Пополнить' }
                             </Button>
                         </div>
                     </div>
                 </div>
                 <Modal
                     isOpen={ isBalanceModalShow }
-                    style={customStyles}
+                    style={ customStyles }
                     onRequestClose={ this.closeBalanceModal }
                 >
-                    <Button
-                        size='m'
-                        view='extra'
-                        width='available'
-                        disabled={ isBalanceLoading }
-                        onClick={ this.handleDeposit }
-                    >
-                        { isBalanceLoading ? 'Идет оплата...' : 'Внести деньги' }
-                    </Button>
+                    <div>
+                        <Input
+                            className='app__modal__input'
+                            clear={ true }
+                            onChange={ this.handleChangeMoneyInput }
+                            label='Введите сумму в ETH'
+                        />
+                        <Button
+                            className='app__modal__button'
+                            size='m'
+                            view='extra'
+                            width='available'
+                            disabled={ isBalanceLoading }
+                            onClick={ this.handleDeposit }
+                        >
+                            { isBalanceLoading ? 'Идет оплата...' : 'Внести деньги' }
+                        </Button>
+                    </div>
+
                 </Modal>
             </div>
         );
